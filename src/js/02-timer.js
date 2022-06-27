c2307e49ac126212f34e226f364cfb8f28febbe9
Sergey Shevchenko <sergeishevchenko111@gmail.com>
2022-06-27 23:55:01 +0300

    upd-09

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,34 +2,97 @@
 import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
-// 1 створюємо клас
-class CountdownTimer {
-  // 4 конструктор
-  constructor({ selector, targetDate }) {
-    this.targetDate = targetDate;
-    this.selector = selector;
-  }
-  // 5 метод, затримка
-  updateTimer() {
-    setInterval(() => {}, 1000);
-  }
-}
-// 2 створюємо екземпляр з об*єктом, який створить новий таймер з налаштуваннями
-const timer = new CountdownTimer({
-  // 3 виклик та ініціалізація
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 209'),
+// Бібліотека дла повідомлень користувачу
+import Notiflix from 'notiflix';
+//
+
+// ініціалізація здеякими параметрами
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-top',
+  closeButton: false,
+  //   distance: '10px',
+  //   opacity: 1,
+  // ...
 });
-// поточна дата
-console.log(new Date());
-// дата в мілісекундах
-console.log(Date.now());
-console.log(Date.now(0));
-// // If using flatpickr in a framework, its recommended to pass the element directly
-// flatpickr(element, {});
 
-// // Otherwise, selectors are also supported
-// flatpickr('#myID', {});
+const date = document.querySelector('#datetime-picker');
+const day = document.querySelector('[data-days]');
+const hour = document.querySelector('[data-hours]');
+const minute = document.querySelector('[data-minutes]');
+const second = document.querySelector('[data-seconds]');
+// звернення до кнопки
+const button = document.querySelector('[data-start]');
+//
+let timerId = null;
+//
+button.disabled = true;
+//
+button.addEventListener('click', updateTimer);
 
-// // creates multiple instances
-// flatpickr('.anotherSelector');
+//
+flatpickr(date, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      //   window.alert('Please choose a date in the future');
+    } else {
+      button.disabled = false;
+
+      // console.log(selectedDates[0]);
+    }
+  },
+});
+
+//
+function updateTimer() {
+  Notiflix.Notify.success('Timer has been started');
+  timerId = setInterval(() => {
+    const currentTime = new Date(date.value);
+    const delta = currentTime - Date.now();
+    // console.log(delta);
+    //
+    let { days, hours, minutes, seconds } = convertMs(delta);
+    //
+    day.textContent = days < 10 ? '0' + days : days;
+    hour.textContent = hours < 10 ? '0' + hours : hours;
+    minute.textContent = minutes < 10 ? '0' + minutes : minutes;
+    second.textContent = seconds < 10 ? '0' + seconds : seconds;
+    //
+    if (delta < 1000) {
+      clearInterval(timerId);
+
+      button.disabled = true;
+    }
+  }, 1000);
+}
+//
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+//
+// function addLeadingZero(value) {
+//     padStart() {
+
+//     };
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+// звертаємося до форми
+const form = document.querySelector('.form');
+// вішаємо слухача
+form.addEventListener('submit', formSubmit);
+//
+function formSubmit(event) {
+  // відміна перезагрузки
+  event.preventDefault();
+  //  деструкторизація
+  const { delay, step, amount } = event.target.elements;
+
+  for (let i = 0; i < amount.value; i += 1) {
+    let position = i + 1;
+
+    let passed = Number(delay.value) + Number(step.value) * i;
+    //  ф-ція яка повертає один проміс
+    createPromise(position, passed)
+      .then(object => {
+        const { position, delay } = object;
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(object => {
+        const { position, delay } = object;
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
+//  ф-ці викликається при сабміті
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+//
